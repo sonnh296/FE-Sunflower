1afd1db07fbe5e1d7252a272d40e71b440b3b0bf
Nguyễn Hồng Sơn <mac2019@192.168.0.100>
2025-05-13 02:09:13 +0700

    fix domain

--- src/constants/env.ts
+++ src/constants/env.ts
@@ -1,5 +1,7 @@
-export const API_BASE_URL = 'http://localhost:8080/api'
-export const BASE_URL = 'http://localhost:8080/api'
-export const NOTIFICATION_HUB = 'http://localhost:8080'
+export const API_BASE_URL =
+  'https://elsun-be-ang8fxdgbuhrdmf3.southeastasia-01.azurewebsites.net/api'
+export const BASE_URL = 'https://elsun-be-ang8fxdgbuhrdmf3.southeastasia-01.azurewebsites.net/api'
+export const NOTIFICATION_HUB =
+  'https://elsun-be-ang8fxdgbuhrdmf3.southeastasia-01.azurewebsites.net'
 export const VITE_APP_GOONG_API_KEY = '9hI3nUIhxsu9GxxA97L7RjF2OlIHVjJxEgxqZ1C3'
 export const VITE_APP_GOONG_MAP_KEY = 'ZdlDGQqjlNt3AKAYCbl7gbD3jwc8gmU96fm7vu5D'
