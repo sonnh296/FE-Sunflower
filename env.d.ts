204de55ef6c4027e0b2b6d45f7b59bdac0688043
Nguyễn Hồng Sơn (KHN.SBU2) <sonnh1@kaopiz.com>
2025-10-22 13:23:35 +0700

    fix build

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+import type { I18n } from 'vue-i18n'
+
+declare module '@vue/runtime-core' {
+  interface ComponentCustomProperties {
+    $t: (key: string, ...args: any[]) => string
+    $i18n: I18n['global']
+  }
+}
